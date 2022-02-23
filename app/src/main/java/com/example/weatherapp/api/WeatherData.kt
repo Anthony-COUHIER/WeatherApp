e71e32dd47171af487005a627f9bccd578239aff
Anthony COUHIER <anthony.couhier@epitech.eu>
2022-02-23 01:31:25 +0100

    feat: item from grid

--- app/src/main/java/com/example/weatherapp/api/WeatherData.kt
+++ app/src/main/java/com/example/weatherapp/api/WeatherData.kt
@@ -2,25 +2,28 @@ package com.example.weatherapp.api
 
 import com.squareup.moshi.Json
 
-data class LocationData(
-    @Json(name = "name") val name: String,
-    @Json(name = "country") val country: String,
-)
-
-data class CurrentData(
-    @Json(name = "temp_c") val temp: Double,
-    @Json(name = "condition") val name: ConditionData,
-    @Json(name = "wind_kph") val wind: Double,
-    @Json(name = "humidity") val humidity: Double,
-) {
-
-    data class ConditionData(
-        @Json(name = "text") val text: String,
-        @Json(name = "icon") val icon: String,
-    )
-}
 
 data class WeatherData(
     @Json(name = "location") val location: LocationData,
     @Json(name = "current") val current: CurrentData,
-)
+) {
+
+    data class LocationData(
+        @Json(name = "name") val name: String,
+        @Json(name = "country") val country: String,
+    )
+
+    data class CurrentData(
+        @Json(name = "temp_c") val temp: Double,
+        @Json(name = "condition") val name: ConditionData,
+        @Json(name = "wind_kph") val wind: Double,
+        @Json(name = "humidity") val humidity: Double,
+    ) {
+
+        data class ConditionData(
+            @Json(name = "text") val text: String,
+            @Json(name = "icon") val icon: String,
+        )
+    }
+
+}
